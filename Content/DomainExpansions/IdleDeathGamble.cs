dc97fab3d604660c54960fb765dd7c4f67aab3e9
ehann <wolfxix106@gmail.com>
2025-03-27 01:04:25 -0400

    smoother animations

--- Content/DomainExpansions/IdleDeathGamble.cs
+++ Content/DomainExpansions/IdleDeathGamble.cs
@@ -28,6 +28,8 @@ namespace sorceryFight.Content.DomainExpansions
         Vector2 pachinkoMachinePosition;
         Texture2D pachinkoMachineTexture;
 
+        ref float pachinkoMachineLoops => ref NPC.ai[2];
+        ref float rollSpeed => ref NPC.ai[3];
 
         public override void SetDefaults()
         {
@@ -73,12 +75,17 @@ namespace sorceryFight.Content.DomainExpansions
                 if (pachinkoMachinePosition.X < -pachinkoMachineTexture.Width)
                 {
                     pachinkoMachinePosition.X = Main.screenWidth;
-                    NPC.ai[2]++;
+                    pachinkoMachineLoops++;
                 }
 
+                if (pachinkoMachineLoops == 0) return;
+
+                rollSpeed ++;
+                if (rollSpeed > 10) rollSpeed = 10;
 
                 if (!rolled)
                 {
+                    rollSpeed = 0;
                     rollOneValue = Main.rand.Next(8) + 1;
                     rollTwoValue = Main.rand.Next(8) + 1;
                     rollThreeValue = Main.rand.Next(8) + 1;
@@ -87,7 +94,7 @@ namespace sorceryFight.Content.DomainExpansions
                     rollTwoTexture = ModContent.Request<Texture2D>($"sorceryFight/Content/DomainExpansions/IdleDeathGambleAssets/{rollTwoValue}", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
                     rollThreeTexture = ModContent.Request<Texture2D>($"sorceryFight/Content/DomainExpansions/IdleDeathGambleAssets/{rollThreeValue}", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
 
-                    if (NPC.ai[2] == 0)
+                    if (pachinkoMachineLoops <= 1)
                     {
                         rollOnePosition = Owners[NPC.whoAmI].Center - Main.screenPosition + new Vector2(-rollOneTexture.Width - 150f, -(Main.screenHeight / 2) - (rollOneTexture.Height / 2));
                         rollTwoPosition = Owners[NPC.whoAmI].Center - Main.screenPosition + new Vector2(0, -(Main.screenHeight / 2) - (rollTwoTexture.Height / 2));
@@ -99,15 +106,18 @@ namespace sorceryFight.Content.DomainExpansions
                         rollTwoPosition = Owners[NPC.whoAmI].Center - Main.screenPosition;
                         rollThreePosition = Owners[NPC.whoAmI].Center - Main.screenPosition + new Vector2(rollThreeTexture.Width + 150f, 0);
                     }
-                    
+
                     rolled = true;
                 }
 
                 if (rollTimer < 240)
                 {
-                    rollOnePosition.Y += 200f;
-                    rollTwoPosition.Y += 200f;
-                    rollThreePosition.Y += 200f;
+                    if (rollTimer < 180)
+                        rollOnePosition.Y += 200f * (0.1f * rollSpeed);
+                    if (rollTimer < 210)
+                        rollTwoPosition.Y += 200f * (0.1f * rollSpeed);
+                    if (rollTimer < 240)
+                        rollThreePosition.Y += 200f * (0.1f * rollSpeed);;
 
                     if (rollOnePosition.Y > Main.screenHeight + rollOneTexture.Height / 2)
                         rollOnePosition.Y = 0 - rollOneTexture.Height / 2;
@@ -119,13 +129,55 @@ namespace sorceryFight.Content.DomainExpansions
 
 
                 if (rollTimer >= 180)
-                    rollOnePosition = Owners[NPC.whoAmI].Center - Main.screenPosition - new Vector2(rollOneTexture.Width + 150, 0);
-                if (rollTimer >= 210)
-                    rollTwoPosition = Owners[NPC.whoAmI].Center - Main.screenPosition;
-                if (rollTimer >= 240)
-                    rollThreePosition = Owners[NPC.whoAmI].Center - Main.screenPosition + new Vector2(rollThreeTexture.Width + 150, 0);
+                {
+                    Vector2 target = Owners[NPC.whoAmI].Center - Main.screenPosition - new Vector2(rollOneTexture.Width + 150, 0);
 
-                if (rollTimer >= 330)
+                    if (rollTimer == 180)
+                        rollOnePosition.Y = target.Y + 200;
+                    else
+                    {
+                        float difference = rollOnePosition.Y - target.Y;
+
+                        rollOnePosition.Y -= difference / 2f;
+
+                        if (rollTimer >= 240)
+                            rollOnePosition = target;
+                    }
+                }
+
+                if (rollTimer >= 210)
+                {
+                    Vector2 target = Owners[NPC.whoAmI].Center - Main.screenPosition;
+                    if (rollTimer == 210)
+                        rollTwoPosition.Y = target.Y + 200;
+                    else
+                    {
+                        float difference = rollTwoPosition.Y - target.Y;
+
+                        rollTwoPosition.Y -= difference / 2f;
+
+                        if (rollTimer >= 270)
+                            rollTwoPosition = target;
+                    }
+                }
+
+                if (rollTimer >= 240)
+                {
+                    Vector2 target = Owners[NPC.whoAmI].Center - Main.screenPosition + new Vector2(rollThreeTexture.Width + 150, 0);
+                    if (rollTimer == 240)
+                        rollThreePosition.Y = target.Y + 200;
+                    else
+                    {
+                        float difference = rollThreePosition.Y - target.Y;
+
+                        rollThreePosition.Y -= difference / 2f;
+
+                        if (rollTimer >= 300)
+                            rollThreePosition = target;
+                    }
+                }
+
+                if (rollTimer >= 390)
                 {
                     if (rollOneValue == rollTwoValue && rollOneValue == rollThreeValue)
                     {
@@ -153,13 +205,13 @@ namespace sorceryFight.Content.DomainExpansions
             Vector2 bgOrigin = new Vector2(BackgroundTexture.Width / 2, BackgroundTexture.Height / 2);
             spriteBatch.Draw(BackgroundTexture, NPC.Center - Main.screenPosition, default, Color.White, NPC.rotation, bgOrigin, BackgroundScale, SpriteEffects.None, 0f);
 
-            if (rolled)
+            if (Main.myPlayer == Owners[NPC.whoAmI].whoAmI)
             {
-                if (Main.myPlayer == Owners[NPC.whoAmI].whoAmI)
+                if (rolled)
                 {
                     DrawRolls(spriteBatch);
-                    DrawPachinkoMachines(spriteBatch);
                 }
+                DrawPachinkoMachines(spriteBatch);
             }
             return false;
         }
@@ -174,7 +226,7 @@ namespace sorceryFight.Content.DomainExpansions
                 spriteBatch.Draw(pachinkoMachineTexture, pachinkoMachinePosition - new Vector2(0, Main.screenHeight - (pachinkoMachineTexture.Height * 2) - (Main.LocalPlayer.height * 2)) + new Vector2(pachinkoMachineTexture.Width * 2 * i, 0), default, Color.White, NPC.rotation + MathHelper.Pi, pachinkoOrigin, 1f, SpriteEffects.None, 0f);
             }
 
-            if (NPC.ai[2] == 0) return;
+            if (pachinkoMachineLoops == 0) return;
 
             for (int i = 0; i < 10; i++)
             {
