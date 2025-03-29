e60edfffbacfd4dd63f4b9ca6d14cf2eef37ba7d
ehann <wolfxix106@gmail.com>
2025-03-29 17:41:59 -0400

    client config

---
+++ SorceryFightClientConfig.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace sorceryFight
+{
+    public class ClientConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [DefaultValue(true)]
+        public bool BlackFlashScreenEffects { get; set; }
+    }
+}
