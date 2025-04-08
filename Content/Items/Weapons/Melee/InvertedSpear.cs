16738ac490f081c2e56394fde87ab58778756a02
ehann <wolfxix106@gmail.com>
2025-04-08 14:58:50 -0400

    inverted spear

---
+++ Content/Items/Weapons/Melee/InvertedSpear.cs
@@ -0,0 +1,73 @@
+using System;
+using CalamityMod;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sorceryFight.Content.CursedTechniques.Shrine;
+using sorceryFight.Rarities;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace sorceryFight.Content.Items.Weapons.Melee
+{
+    public class InvertedSpear : ModItem
+    {
+        private static Texture2D texture;
+
+        public override LocalizedText DisplayName => SFUtils.GetLocalization("Mods.sorceryFight.Weapons.Melee.InvertedSpear.DisplayName");
+        public override LocalizedText Tooltip => SFUtils.GetLocalization("Mods.sorceryFight.Weapons.Melee.InvertedSpear.Tooltip");
+
+        public override void SetStaticDefaults()
+        {
+            if (Main.dedServ) return;
+            texture = ModContent.Request<Texture2D>("sorceryFight/Content/Items/Weapons/Melee/InvertedSpear", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 75;
+            Item.height = 90;
+            Item.maxStack = 1;
+            Item.useTime = 1;
+            Item.damage = 700;
+            Item.knockBack = 5;
+            Item.useAnimation = 1;
+            Item.noUseGraphic = true;
+            Item.rare = ModContent.RarityType<SorceryFightRed>();
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shoot = ModContent.ProjectileType<InvertedSpearSlash>();
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.noMelee = true;
+            Item.ArmorPenetration = 1000;
+        }
+
+        public override void ModifyWeaponCrit(Player player, ref float crit) => crit = 1;
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frameRect, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            spriteBatch.Draw(texture, position, frameRect, Color.White, 0f, origin, scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            spriteBatch.Draw(texture, Item.Center - Main.screenPosition, null, lightColor, rotation, texture.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, player.Center, velocity, type, damage, knockback, player.whoAmI);
+            return false;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.ownedProjectileCounts[Item.shoot] > 0)
+                return false;
+            return true;
+        }
+    }
+}
