306404800c26feada9dcfb9b62826aee0fdc007f
ehann <wolfxix106@gmail.com>
2025-05-11 03:12:20 -0400

    domain expansion documentation

--- Content/DomainExpansions/DomainExpansion.cs
+++ Content/DomainExpansions/DomainExpansion.cs
@@ -20,18 +20,69 @@ namespace sorceryFight.Content.DomainExpansions
         public string DisplayName => SFUtils.GetLocalizationValue($"Mods.sorceryFight.DomainExpansions.{InternalName}.DisplayName");
         public string Description => SFUtils.GetLocalizationValue($"Mods.sorceryFight.DomainExpansions.{InternalName}.Description");
         public string LockedDescription => SFUtils.GetLocalizationValue($"Mods.sorceryFight.DomainExpansions.{InternalName}.LockedDiscription");
+
+        /// <summary>
+        /// The sound played when the domain is first casted (after the "Domain Expansion: _________" text plays).
+        /// </summary>
         public abstract SoundStyle CastSound { get; }
+
+        /// <summary>
+        /// The texture of the domain.
+        /// </summary>
         public abstract Texture2D DomainTexture { get; }
+
+        /// <summary>
+        /// The sure hit range of the domain. Also used to draw DrawInnerDomain(Spritebatch spriteBatch) for players inside the range.
+        /// </summary>
         public abstract float SureHitRange { get; }
+    
+        /// <summary>
+        /// The CE cost per second of the domain.
+        /// </summary>
         public abstract float Cost { get; }
+
+        /// <summary>
+        /// Whether or not the domain is closed. Used to determine if players and NPCs can leave/enter the sure hit radius.
+        /// </summary>
         public abstract bool ClosedDomain { get; }
+
+        /// <summary>
+        /// The unlock condition of the domain.
+        /// </summary>
+        /// <param name="sf"></param>
+        /// <returns>Whether or not the domain is unlocked</returns>
         public abstract bool Unlocked(SorceryFightPlayer sf);
+
+        /// <summary>
+        /// Used to apply effects to NPCs in the sure hit radius.
+        /// </summary>
+        /// <param name="npc"></param>
         public abstract void SureHitEffect(NPC npc);
+
+        /// <summary>
+        /// Main draw method for the domain.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
         public abstract void Draw(SpriteBatch spriteBatch);
+
+        /// <summary>
+        /// Whether or not the domain targets NPCs. ***DO NOT IN USE AS OF 1.3.6***
+        /// </summary>
         public virtual bool targetsNPCs { get; } = false;
 
+        /// <summary>
+        /// The player.whoAmI of the caster of the domain.
+        /// </summary>
         public int owner;
+
+        /// <summary>
+        /// The center of the domain.
+        /// </summary>
         public Vector2 center;
+
+        /// <summary>
+        /// Runs any logic that needs to be constantly updated. Call base.Update() to auto-calculate SureHitEffect(NPC npc), drain the owner's CE, and disallow entering/leaving the domain.
+        /// </summary>
         public virtual void Update()
         {
             foreach (NPC npc in Main.npc)
@@ -59,11 +110,13 @@ namespace sorceryFight.Content.DomainExpansions
                 }
             }
 
-            DomainBarrier();
+            if (ClosedDomain)
+                DomainBarrier();
         }
 
-        public virtual void SetDefaults() {}
-
+        /// <summary>
+        /// Prevents any player or NPC from entering or leaving the domain.
+        /// </summary>
         public virtual void DomainBarrier()
         {
             foreach (NPC npc in Main.ActiveNPCs)
@@ -127,6 +180,10 @@ namespace sorceryFight.Content.DomainExpansions
             }
         }
 
+        /// <summary>
+        /// Draws on the players screen if they are in the sure hit radius.
+        /// </summary>
+        /// <param name="action"></param>
         public virtual void DrawInnerDomain(Action action)
         {
             if (Vector2.DistanceSquared(Main.LocalPlayer.Center, this.center) <= SureHitRange.Squared())
@@ -135,6 +192,12 @@ namespace sorceryFight.Content.DomainExpansions
             }
         }
 
+        
+        /// <summary>
+        /// Closes the domain. If in multiplayer, sends a packet to the server and all other clients to close the domain on their end.
+        /// </summary>
+        /// <param name="sf"></param>
+        /// <param name="supressSyncPacket"></param>
         public virtual void CloseDomain(SorceryFightPlayer sf, bool supressSyncPacket = false)
         {
             if (Main.myPlayer == sf.Player.whoAmI)
