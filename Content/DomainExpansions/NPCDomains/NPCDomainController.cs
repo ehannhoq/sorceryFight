696c7869b06ca2bcfcdfaa04ca8e04ddb766bc2b
ehann <wolfxix106@gmail.com>
2025-05-20 04:38:34 -0400

    uncommented entire file lmao

--- Content/DomainExpansions/NPCDomains/NPCDomainController.cs
+++ Content/DomainExpansions/NPCDomains/NPCDomainController.cs
@@ -1,85 +1,85 @@
-// using System.Linq;
-// using Microsoft.Xna.Framework;
-// using Terraria;
-// using Terraria.ID;
-// using Terraria.ModLoader;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
-// namespace sorceryFight.Content.DomainExpansions.NPCDomains
-// {
-//     internal static class NPCDomainUtils
-//     {
-//         internal static NPCDomainExpansion GetDomain(this NPC npc)
-//         {
-//             return npc.type switch
-//             {
-//                 NPCID.CultistBoss => new PhantasmicLabyrinth(),
-//                 _ => null
-//             };
-//         }
-//     }
-//     /// <summary>
-//     /// Controls NPC domains. Runs on ALL clients and the server.
-//     /// </summary>
-//     public class NPCDomainController : GlobalNPC
-//     {
-//         public static bool npcIsCastingDomain;
-//         public static Vector2 npcCastingPosition;
-//         public static bool castingDomain;
-
-
-//         public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.GetDomain() != null && lateInstantiation;
-
-//         public override bool PreAI(NPC npc)
-//         {
-//             base.PreAI(npc);
-
-//             if (npc.type == NPCID.CultistBoss) return LunaticCultistDomainController(npc);
-
-//             return true;
-//         }
-
-//         private bool LunaticCultistDomainController(NPC npc)
-//         {
-//             if (npc.active && npc.life >= npc.lifeMax * 0.01f && npc.life < npc.lifeMax * 0.10f && !DomainExpansionController.ActiveDomains.Any(domain => domain.owner == npc.whoAmI))
-//             {
-//                 npcIsCastingDomain = true;
-//                 if (npcCastingPosition == Vector2.Zero)
-//                     npcCastingPosition = npc.Center;
+namespace sorceryFight.Content.DomainExpansions.NPCDomains
+{
+    internal static class NPCDomainUtils
+    {
+        internal static NPCDomainExpansion GetDomain(this NPC npc)
+        {
+            return npc.type switch
+            {
+                NPCID.CultistBoss => new PhantasmicLabyrinth(),
+                _ => null
+            };
+        }
+    }
+    /// <summary>
+    /// Controls NPC domains. Runs on ALL clients and the server.
+    /// </summary>
+    public class NPCDomainController : GlobalNPC
+    {
+        public static bool npcIsCastingDomain;
+        public static Vector2 npcCastingPosition;
+        public static bool castingDomain;
 
 
-//                 npc.Center = npcCastingPosition;
+        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.GetDomain() != null && lateInstantiation;
 
-//                 if (!castingDomain)
-//                 {
-//                     castingDomain = true;
-//                     int index;
-//                     index = CombatText.NewText(npc.getRect(), Color.White, "Domain Expansion:");
-//                     Main.combatText[index].lifeTime = 90;
+        // public override bool PreAI(NPC npc)
+        // {
+        //     base.PreAI(npc);
 
-//                     TaskScheduler.Instance.AddDelayedTask(() =>
-//                     {
-//                         index = CombatText.NewText(npc.getRect(), Color.White, "Phantasmic Labyrinth");
-//                         Main.combatText[index].lifeTime = 90;
-//                     }, 100);
+        //     if (npc.type == NPCID.CultistBoss) return LunaticCultistDomainController(npc);
 
-//                     TaskScheduler.Instance.AddDelayedTask(() =>
-//                     {
-//                         DomainExpansionController.ExpandDomain(npc.whoAmI, npc.GetDomain());
-//                     }, 200);
+        //     return true;
+        // }
 
-//                     TaskScheduler.Instance.AddDelayedTask(() =>
-//                     {
-//                         castingDomain = false;
-//                         npcIsCastingDomain = false;
-//                         npcCastingPosition = Vector2.Zero;
-//                     }, 400);
-//                 }
+        private bool LunaticCultistDomainController(NPC npc)
+        {
+            if (npc.active && npc.life >= npc.lifeMax * 0.01f && npc.life < npc.lifeMax * 0.10f && !DomainExpansionController.ActiveDomains.Any(domain => domain.owner == npc.whoAmI))
+            {
+                npcIsCastingDomain = true;
+                if (npcCastingPosition == Vector2.Zero)
+                    npcCastingPosition = npc.Center;
 
-//                 if (castingDomain) return false;
 
-//             }
+                npc.Center = npcCastingPosition;
 
-//             return true;
-//         }
-//     }
-// }
+                if (!castingDomain)
+                {
+                    castingDomain = true;
+                    int index;
+                    index = CombatText.NewText(npc.getRect(), Color.White, "Domain Expansion:");
+                    Main.combatText[index].lifeTime = 90;
+
+                    TaskScheduler.Instance.AddDelayedTask(() =>
+                    {
+                        index = CombatText.NewText(npc.getRect(), Color.White, "Phantasmic Labyrinth");
+                        Main.combatText[index].lifeTime = 90;
+                    }, 100);
+
+                    TaskScheduler.Instance.AddDelayedTask(() =>
+                    {
+                        DomainExpansionController.ExpandDomain(npc.whoAmI, npc.GetDomain());
+                    }, 200);
+
+                    TaskScheduler.Instance.AddDelayedTask(() =>
+                    {
+                        castingDomain = false;
+                        npcIsCastingDomain = false;
+                        npcCastingPosition = Vector2.Zero;
+                    }, 400);
+                }
+
+                if (castingDomain) return false;
+
+            }
+
+            return true;
+        }
+    }
+}
