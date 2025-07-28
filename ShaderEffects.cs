19da2295aba8635bb52fe439aaf1804423f996db
ehann <wolfxix106@gmail.com>
2025-07-28 03:21:29 -0400

    shader loading

---
+++ ShaderEffects.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.ModLoader;
+
+namespace sorceryFight
+{
+    public class ShaderEffects : ModSystem
+    {
+
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                Asset<Effect> hollowNukeCollision = Mod.Assets.Request<Effect>("Content/Shaders/HollowNukeCollision", AssetRequestMode.ImmediateLoad);
+                Filters.Scene["SF:HollowNuke"] = new Filter(new Terraria.Graphics.Shaders.ScreenShaderData(hollowNukeCollision, "WhiteFade"), EffectPriority.VeryHigh);
+                Filters.Scene["SF:HollowNuke"].Load();
+            }
+        }
+    }
+} 
