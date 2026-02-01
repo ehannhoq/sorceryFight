8a310f07e15486225ca8132e953698505e3eb0f2
Ehan Haque <ehann.hoq@gmail.com>
2026-02-01 02:52:19 -0500

    added reverse charm functionality

--- Content/Items/Accessories/ReverseCharm.cs
+++ Content/Items/Accessories/ReverseCharm.cs
@@ -1,5 +1,9 @@
+using CalamityMod.Items.Materials;
+using sorceryFight.Content.Items.Materials;
 using sorceryFight.Rarities;
+using sorceryFight.SFPlayer;
 using Terraria;
+using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
@@ -7,22 +11,33 @@ namespace sorceryFight.Content.Items.Accessories
 {
     public class ReverseCharm : ModItem
     {
+        public static readonly int additionalHeal = 12;
+        public static readonly float additionalEfficiency = 0.10f;
+    
         public override LocalizedText DisplayName => SFUtils.GetLocalization("Mods.sorceryFight.Accessories.ReverseCharm.DisplayName");
-        public override LocalizedText Tooltip => SFUtils.GetLocalization("Mods.sorceryFight.Accessories.ReverseCharm.Tooltip");
+        public override LocalizedText Tooltip => SFUtils.GetLocalization("Mods.sorceryFight.Accessories.ReverseCharm.Tooltip").WithFormatArgs(additionalHeal, (int)(additionalEfficiency * 100));
 
         public override void SetDefaults()
         {
             Item.accessory = true;
             Item.maxStack = 1;
             Item.rare = ModContent.RarityType<SorceryFightAccessory>();
-            Item.width = 30;
-            Item.height = 30;
-            Item.accessory = true;
+            Item.width = 54;
+            Item.height = 50;
         }
 
         public override bool CanEquipAccessory(Player player, int slot, bool modded)
         {
             return !player.SorceryFight().heavenlyRestriction;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+
+            SorceryFightPlayer sfPlayer = player.SorceryFight();
+            sfPlayer.additionalRCTHealPerSecond += additionalHeal; 
+            sfPlayer.rctEfficiency += additionalEfficiency;
         }
     }
 }
