0cd5166e72dd161a159b95b622f4a82c4c191ed8
Ehan Haque <ehann.hoq@gmail.com>
2026-02-09 19:57:36 -0500

    fixed black flash detection

--- Content/Quests/BlackFlashQuestI.cs
+++ Content/Quests/BlackFlashQuestI.cs
@@ -1,5 +1,5 @@
 using System.Linq;
-using CalamityMod.Items.Placeables.Furniture.Paintings;
+using CalamityMod.Items.Placeables.Furniture.DevPaintings;
 using sorceryFight.Content.Items.Accessories;
 using sorceryFight.SFPlayer;
 using Terraria.ModLoader;
@@ -11,7 +11,20 @@ namespace sorceryFight.Content.Quests
         private const int BLACK_FLASH_COUNT = 3;
         public override bool IsCompleted(SorceryFightPlayer sfPlayer)
         {
-            return sfPlayer.blackFlashCounter >= BLACK_FLASH_COUNT && sfPlayer.Player.inventory.Any(item => item.type == ModContent.ItemType<ThankYouPainting>());
+            int count = 0;
+            if (sfPlayer.blackFlashCounter == -59)
+            {
+                if (sfPlayer.TryGetQuestData(this, "BlackFlashCounter", out object obj))
+                {
+                    count = (int)obj;
+                    count++;
+                    sfPlayer.ModifyQuestData(this, "BlackFlashCounter", count);
+                }
+                else
+                    sfPlayer.ModifyQuestData(this, "BlackFlashCounter", 1);
+            }
+
+            return count >= BLACK_FLASH_COUNT && sfPlayer.Player.inventory.Any(item => item.type == ModContent.ItemType<ThankYouPainting>());
         }
         public override void GiveRewards(SorceryFightPlayer sfPlayer)
         {
