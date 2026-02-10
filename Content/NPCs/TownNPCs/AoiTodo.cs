0c1765fd6c5b63e826e96b56acd8c4f8af1a18e3
Ehan Haque <ehann.hoq@gmail.com>
2026-02-09 19:59:00 -0500

    added aoi todo npc

---
+++ Content/NPCs/TownNPCs/AoiTodo.cs
@@ -0,0 +1,44 @@
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace sorceryFight.Content.NPCs.TownNPCs
+{
+    [AutoloadHead]
+    public class AoiTodo : SorceryFightNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 26;
+            NPCID.Sets.ExtraFramesCount[Type] = 9;
+            NPCID.Sets.AttackFrameCount[Type] = 4;
+            NPCID.Sets.DangerDetectRange[Type] = 700;
+            NPCID.Sets.AttackType[Type] = 0;
+            NPCID.Sets.AttackTime[Type] = 90;
+            NPCID.Sets.AttackAverageChance[Type] = 30;
+            NPCID.Sets.HatOffsetY[Type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            SFNPC.name = "AoiTodo";
+            SFNPC.attackDamage = 50;
+            SFNPC.knockback = 5f;
+            SFNPC.attackCooldown = 30;
+            SFNPC.attackProjectile = 0;
+
+            NPC.defense = 30;
+            NPC.lifeMax = 350;
+            NPC.knockBackResist = 0.75f;
+            AnimationType = NPCID.Guide;
+
+            base.SetDefaults();
+        }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs)
+        {
+            return Main.hardMode;
+        }
+    }
+}
