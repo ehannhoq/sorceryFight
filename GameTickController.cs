f7cd5518523c807cbfe7decfb703517bb2cbcf08
Ehan Haque <ehann.hoq@gmail.com>
2026-03-16 22:30:49 -0400

    (broken) system that slows down tick speed, probably going to use for projection sorcery

---
+++ GameTickController.cs
@@ -0,0 +1,37 @@
+using System;
+using CalamityMod.World.Planets;
+using Microsoft.Xna.Framework;
+using sorceryFight.SFPlayer;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace sorceryFight.Content
+{
+    public class GameTickController : ModSystem
+    {
+        private static float GameSpeed = 1.0f;
+        public override void Load()
+        {
+            On_Main.DoUpdate += DoUpdate;
+        }
+
+        public override void PostUpdateEverything()
+        {
+            GameSpeed = 1.0f;
+            if (SFKeybinds.Test.Current)
+            {
+                GameSpeed = 0.5f;
+            }
+        }
+
+        private void DoUpdate(On_Main.orig_DoUpdate orig, Main self, ref GameTime gameTime)
+        {
+            var scaledTime = new GameTime(
+                gameTime.TotalGameTime,
+                TimeSpan.FromTicks((long)(gameTime.ElapsedGameTime.Ticks * GameSpeed))
+            );
+            orig(self, ref scaledTime);
+        }
+    }
+}
