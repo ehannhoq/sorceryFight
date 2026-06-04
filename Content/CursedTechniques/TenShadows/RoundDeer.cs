6bbe1ff583ab72b5a454f1927b5ab1f918648691
ehann <ehann.hoq@gmail.com>
2026-06-03 20:45:48 -0400

    removed unnecesary import

--- Content/CursedTechniques/TenShadows/RoundDeer.cs
+++ Content/CursedTechniques/TenShadows/RoundDeer.cs
@@ -8,7 +8,6 @@ using Terraria.Audio;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
-using sorceryFight.Utilities;
 
 namespace sorceryFight.Content.CursedTechniques.TenShadows
 {
