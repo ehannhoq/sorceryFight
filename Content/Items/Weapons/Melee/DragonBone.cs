4149e2db73c2a0ab69dfceea4fe79e6705889be6
ehann <ehann.hoq@gmail.com>
2026-08-02 04:38:01 -0400

    added dragon bone

---
+++ Content/Items/Weapons/Melee/DragonBone.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sorceryFight.Content.Projectiles.Melee;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace sorceryFight.Content.Items.Weapons.Melee
+{
+    public class DragonBone : ModItem
+    {
+        public override LocalizedText DisplayName => SFUtils.GetLocalization("Mods.sorceryFight.Weapons.Melee.DragonBone.DisplayName");
+        public override LocalizedText Tooltip => SFUtils.GetLocalization("Mods.sorceryFight.Weapons.Melee.DragonBone.Tooltip");
+
+        public override void SetDefaults()
+        {
+            Item.width = 29;
+            Item.height = 29;
+            Item.maxStack = 1;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.damage = 56;
+            Item.crit = 11;
+            Item.knockBack = 8;
+            Item.autoReuse = true;
+            Item.DamageType = CursedTechniqueDamageClass.Instance;
+            Item.shoot = ModContent.ProjectileType<DragonBoneSlash>();
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item7;
+            Item.channel = true;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.useTurn = true;
+            Item.rare = ItemRarityID.Pink;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.ownedProjectileCounts[Item.shoot] > 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Content/Projectiles/Melee/DragonBoneSlash.cs
@@ -0,0 +1,88 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sorceryFight.Content.Particles;
+using sorceryFight.Content.Particles.UIParticles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace sorceryFight.Content.Projectiles.Melee
+{
+    public class DragonBoneSlash : ModProjectile
+    {
+        private static Texture2D texture;
+        private const int FRAME_COUNT = 11;
+        private const int TICKS_PER_FRAME = 2;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = FRAME_COUNT;
+            if (Main.dedServ) return;
+            texture = ModContent.Request<Texture2D>("sorceryFight/Content/Projectiles/Melee/DragonBoneSlash", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 350;
+            Projectile.height = 250;
+            Projectile.DamageType = CursedTechniqueDamageClass.Instance;
+            Projectile.timeLeft = 300;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.penetrate = -1;
+            Projectile.ownerHitCheck = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 12;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.PositionProjectileForSlash(20f);
+        }
+
+        public override void AI()
+        {
+            Projectile.HandleProjectileAnimation(FRAME_COUNT, TICKS_PER_FRAME);
+            Projectile.PositionProjectileForSlash(20f);
+
+            // Player player = Main.player[Projectile.owner];
+            // float swingProgress = (float)Projectile.frame / (FRAME_COUNT - 1);
+            // float armRotation = MathHelper.Lerp(-MathHelper.PiOver2, MathHelper.PiOver2, swingProgress);
+
+            // player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Quarter, (armRotation - MathHelper.PiOver2) * player.direction);
+            // player.itemRotation = armRotation;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            int frameHeight = texture.Height / FRAME_COUNT;
+            int frameY = Projectile.frame * frameHeight;
+
+            Rectangle sourceRectangle = new Rectangle(0, frameY, texture.Width, frameHeight);
+            Vector2 projOrigin = sourceRectangle.Size() * 0.5f;
+
+            SpriteEffects spriteEffects = Projectile.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(
+                SpriteSortMode.Immediate,
+                BlendState.NonPremultiplied,
+                SamplerState.LinearClamp,
+                DepthStencilState.None,
+                RasterizerState.CullNone,
+                null,
+                Main.GameViewMatrix.ZoomMatrix
+            );
+
+            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, sourceRectangle, Color.White, Projectile.rotation, projOrigin, Projectile.scale, spriteEffects, 0f);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin();
+
+            return false;
+        }
+    }
+}
