bb58c4e4aeeabad2255f77ee1115d94e20e50cff
ehann <ehann.hoq@gmail.com>
2026-08-22 03:25:09 -0400

    fixed gakuganji sukuna finger drop conditions

--- Content/Shops/SukunasFingersShop.cs
+++ Content/Shops/SukunasFingersShop.cs
@@ -12,39 +12,26 @@ namespace sorceryFight.Content.Shops
         {
             ShopName = "SukunasFingersShop";
 
-            // Condition downedHiveMindOrPerforator = CalamityConditions.DownedHiveMindOrPerforator;
-            // Condition downedCalamitasClone = CalamityConditions.DownedCalamitasClone;
-            // Condition downedLeviathan = CalamityConditions.DownedLeviathan;
-            // Condition downedRavager = CalamityConditions.DownedRavager;
-            // Condition downedAstrumDeus = CalamityConditions.DownedAstrumDeus;
-            // Condition downedDragonfolly = CalamityConditions.DownedBumblebird;
-            // Condition downedProvidence = CalamityConditions.DownedProvidence;
-            // Condition downedCeaselessVoid = CalamityConditions.DownedCeaselessVoid;
-            // Condition downedStormWeaver = CalamityConditions.DownedStormWeaver;
-            // Condition downedSignus = CalamityConditions.DownedStormWeaver;
-            // Condition downedPolterghast = CalamityConditions.DownedPolterghast;
-            // Condition downedDoG = CalamityConditions.DownedStormWeaver;
-            
-            AddItem(ModContent.ItemType<SukunasFingerI>(), Item.buyPrice(silver: 50), Condition.DownedEyeOfCthulhu);
-            // AddItem(ModContent.ItemType<SukunasFingerII>(), Item.buyPrice(gold: 1), downedHiveMindOrPerforator);
-            AddItem(ModContent.ItemType<SukunasFingerIII>(), Item.buyPrice(gold: 1, silver: 50), Condition.DownedSkeletron);
-            AddItem(ModContent.ItemType<SukunasFingerIV>(), Item.buyPrice(gold: 2), Condition.Hardmode);
-            AddItem(ModContent.ItemType<SukunasFingerV>(), Item.buyPrice(gold: 3), Condition.DownedSkeletronPrime);
-            // AddItem(ModContent.ItemType<SukunasFingerVI>(), Item.buyPrice(gold: 4), downedCalamitasClone);
-            AddItem(ModContent.ItemType<SukunasFingerVII>(), Item.buyPrice(gold: 5), Condition.DownedPlantera);
-            // AddItem(ModContent.ItemType<SukunasFingerVIII>(), Item.buyPrice(gold: 7), downedLeviathan);
-            AddItem(ModContent.ItemType<SukunasFingerIX>(), Item.buyPrice(gold: 9), Condition.DownedGolem);
-            // AddItem(ModContent.ItemType<SukunasFingerX>(), Item.buyPrice(gold: 12), downedRavager);
-            AddItem(ModContent.ItemType<SukunasFingerXI>(), Item.buyPrice(gold: 16), Condition.DownedCultist);
-            // AddItem(ModContent.ItemType<SukunasFingerXII>(), Item.buyPrice(gold: 21), downedAstrumDeus);
-            AddItem(ModContent.ItemType<SukunasFingerXIII>(), Item.buyPrice(gold: 28), Condition.DownedMoonLord);
-            // AddItem(ModContent.ItemType<SukunasFingerXIV>(), Item.buyPrice(gold: 35), downedDragonfolly);
-            // AddItem(ModContent.ItemType<SukunasFingerXV>(), Item.buyPrice(gold: 42), downedProvidence);
-            // AddItem(ModContent.ItemType<SukunasFingerXVI>(), Item.buyPrice(gold: 47), downedCeaselessVoid);
-            // AddItem(ModContent.ItemType<SukunasFingerXVII>(), Item.buyPrice(gold: 53), downedStormWeaver);
-            // AddItem(ModContent.ItemType<SukunasFingerXVIII>(), Item.buyPrice(gold: 59), downedSignus);
-            // AddItem(ModContent.ItemType<SukunasFingerXIX>(), Item.buyPrice(gold: 68), downedPolterghast);
-            // AddItem(ModContent.ItemType<SukunasFingerXX>(), Item.buyPrice(gold: 75), downedDoG);
+            AddItem(ModContent.ItemType<SukunasFingerI>(), Item.buyPrice(silver: 50), Condition.DownedKingSlime);
+            AddItem(ModContent.ItemType<SukunasFingerII>(), Item.buyPrice(gold: 1), Condition.DownedEyeOfCthulhu);
+            AddItem(ModContent.ItemType<SukunasFingerIII>(), Item.buyPrice(gold: 1, silver: 50), Condition.DownedEowOrBoc);
+            AddItem(ModContent.ItemType<SukunasFingerIV>(), Item.buyPrice(gold: 2), Condition.DownedQueenBee);
+            AddItem(ModContent.ItemType<SukunasFingerV>(), Item.buyPrice(gold: 3), Condition.DownedSkeletron);
+            AddItem(ModContent.ItemType<SukunasFingerVI>(), Item.buyPrice(gold: 4), Condition.DownedDeerclops);
+            AddItem(ModContent.ItemType<SukunasFingerVII>(), Item.buyPrice(gold: 5), Condition.Hardmode);
+            AddItem(ModContent.ItemType<SukunasFingerVIII>(), Item.buyPrice(gold: 7), SFConditions.DownedFingerBearerI);
+            AddItem(ModContent.ItemType<SukunasFingerIX>(), Item.buyPrice(gold: 9), Condition.DownedQueenSlime);
+            AddItem(ModContent.ItemType<SukunasFingerX>(), Item.buyPrice(gold: 12), Condition.DownedTwins);
+            AddItem(ModContent.ItemType<SukunasFingerXI>(), Item.buyPrice(gold: 16), Condition.DownedDestroyer);
+            AddItem(ModContent.ItemType<SukunasFingerXII>(), Item.buyPrice(gold: 21), Condition.DownedSkeletronPrime);
+            AddItem(ModContent.ItemType<SukunasFingerXIII>(), Item.buyPrice(gold: 28), Condition.DownedPlantera);
+            AddItem(ModContent.ItemType<SukunasFingerXIV>(), Item.buyPrice(gold: 35), Condition.DownedIceQueen);
+            AddItem(ModContent.ItemType<SukunasFingerXV>(), Item.buyPrice(gold: 42), Condition.DownedPumpking);
+            AddItem(ModContent.ItemType<SukunasFingerXVI>(), Item.buyPrice(gold: 47), SFConditions.DownedFingerBearerII);
+            AddItem(ModContent.ItemType<SukunasFingerXVII>(), Item.buyPrice(gold: 53), Condition.DownedDukeFishron);
+            AddItem(ModContent.ItemType<SukunasFingerXVIII>(), Item.buyPrice(gold: 59), Condition.DownedEmpressOfLight);
+            AddItem(ModContent.ItemType<SukunasFingerXIX>(), Item.buyPrice(gold: 68), Condition.DownedCultist);
+            AddItem(ModContent.ItemType<SukunasFingerXX>(), Item.buyPrice(gold: 75), Condition.DownedMoonLord);
         }
     }
 }
